0cf2049f73ed92c533eb6dae221ea74fcec2e979
cj <cj@HP-2009>
2013-06-10 01:44:19 +0530

    made modifications to reduce code

--- includes/js/makeForm.js
+++ includes/js/makeForm.js
@@ -1,28 +1,45 @@
 function createRow(value,id)
 {
+    var inputField = '<input type="number" name="item['+id+'][quantity]" value="0" required max="3" pattern="[0-9]+"><input type="hidden" name="item['+id+'][data]" value="'+value+'" required></td>';
     var row = '<tr>';
     row += '<td class="name">'+value[0]+'</td>';
-    row += '<td>'+value[1]+'</td>';
-    row += '<td>'+value[2]+'</td>';
-    row += '<td>'+value[3]+'</td>';
-    row += '<td><input type="number" name="item['+id+'][quantity]" value="0" required>';
-    row += '<input type="hidden" name="item['+id+'][data]" value="'+value+'" required></td>';
+    row += createCell(0,value[1]);
+    row += createCell(0,value[2]);
+    row += createCell(0,value[3]);
+    row += createCell(0,inputField);
     row += '</tr>'
     return row;
 }
+function createCell(type,value)
+{
+    if(type===1)
+    {
+        return '<th>'+value+'</th>';
+    }
+    else
+    {
+        return '<td>'+value+'</td>';
+    }
+}
 function head()
 {
-    var row = '<tr>';
-    row += '<th><input class="search"></th>';
-    row += '<th>Barcode</th>';
-    row += '<th>Price</th>';
-    row += '<th>MRP</th>';
-    row += '<th>Quantity</th>';
-    row += '<input type="hidden" name="op" value="printList"><input type="hidden" name="cName" value="'+cName+'">';
-    row += '</tr>';
-    return row;
+    var hiddenVal = '<input type="hidden" name="op" value="printList"><input type="hidden" name="cName" value="'+cName+'">';
+    var filterField = '<input class="search" placeholder="Filter By Name" size="50px">';
+    
+    var topRow = '<tr>';
+    topRow += '<td colspan="100%" align="center">Filter Values : '+filterField+hiddenVal+'</td>';
+    topRow += '</tr>';
+    
+    var headRow = '<tr>';
+    headRow += createCell(1,'Name');
+    headRow += createCell(1,'Barcode');
+    headRow += createCell(1,'Price');
+    headRow += createCell(1,'MRP');
+    headRow += createCell(1,'Quantity - Labels Needed');
+    headRow += '</tr>';
+    return topRow + headRow;
 }
-function body()
+function body(values)
 {
     var row;
     for (var i = 0;i<values.length;i++)
@@ -34,20 +51,21 @@ function body()
 }
 function foot()
 {
-    var row = '<tr><td colspan="100"align="center"><input type="submit" value="Print BarCodes"></td></tr>';
+    var printButton = '<input type="submit" value="Print BarCodes">';
+    var row = '<tr><td colspan="100%"align="center">'+printButton+'</td></tr>';
     return row;
 }
 function addSorter()
 {
     var options = { valueNames: ['name']};
     var poList = new List('PO', options);
-    var inputPO = 'Enter PO Number: <input type="text" name="poNo" placeholder="Enter PO No" required><br><br>';
+    var inputPO = 'Enter PO Number : <input type="text" name="poNo" placeholder="Enter PO No" required maxlength="5"><br><br>';
     $('#PO').prepend(inputPO);
 }
 function createForm(values,cName)
 {
     $('#appendList').find('thead').html(head());
-    $('#appendList').find('tbody').html(body());
+    $('#appendList').find('tbody').html(body(values));
     $('#appendList').find('tfoot').html(foot());
     addSorter();
     return 1;
