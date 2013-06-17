aeb1f9cdad09633943eb773cafd23e343a36824b
cj <cj@HP-2009>
2013-06-17 14:55:15 +0530

    minor modification

--- includes/prnLay.php
+++ includes/prnLay.php
@@ -13,12 +13,12 @@ function prnGenerator($list)
             $quantity = $item[5];
             $company = $item[6];
 
-            $a .= "A3,4,0,1,1,1,N,\"{$product}\"\r\n";
-            $a .= "A28,30,1,4,1,1,N,\"{$company}\"\r\n";
-            $a .= "B35,21,0,1,2,6,46,B,\"{$barcode}\"\r\n";
-            $a .= "A35,99,0,2,2,2,N,\"Rs{$price}\"\r\n";
-            $a .= "A182,98,0,2,1,1,N,\" MRP\"\r\n";
-            $a .= "A182,114,0,2,1,1,N,\"Rs{$mrp}\"\r\n";
+            $a .= "A9,1,0,1,1,1,N,\"{$product}\"\r\n";
+            $a .= "A31,20,1,4,1,1,N,\"{$company}\"\r\n";
+            $a .= "B45,18,0,1,2,6,49,B,\"{$barcode}\"\r\n";
+            $a .= "A45,103,0,4,1,1,N,\"Rs{$price}\"\r\n";
+            $a .= "A203,92,0,4,1,1,N,\" MRP\"\r\n";
+            $a .= "A203,114,0,4,1,1,N,\"Rs{$mrp}\"\r\n";
             $a .= "P{$quantity}\r\n";
             $prnFiles[] = $a;
     }
@@ -27,15 +27,15 @@ function prnGenerator($list)
 function prnFixedCharacters()
 {
     $a = "I8,A,001\r\n\r\n\r\n";
-    $a .= "Q160,008\r\n";
+    $a .= "Q160,024\r\n";
     $a .= "q863\r\n";
     $a .= "rN\r\n";
-    $a .= "S2\r\n";
-    $a .= "D10\r\n";
+    $a .= "S4\r\n";
+    $a .= "D12\r\n";
     $a .= "ZT\r\n";
     $a .= "JF\r\n";
-    $a .= "OP\r\n";
-    $a .= "R279,0\r\n";
+    $a .= "O\r\n";
+    $a .= "R271,0\r\n";
     $a .= "f100\r\n";
     $a .= "N\r\n";
     return $a;
