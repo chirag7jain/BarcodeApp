7304561ef61c07b0a141b6ea18599941afbc8962
Chirag Jain <chirag7jain@gmail.com>
2013-06-18 09:46:00 +0530

    Removed Zipping process. Now just combining multiple prn files into one prn file and sending it to user

--- includes/etcLay.php
+++ includes/etcLay.php
@@ -5,42 +5,18 @@ function createBat($PC='SAMSUNG-2012',$PRINTER = 'ZEBRA')
     $batCommand .= "COPY *.prn \\\\{$PC}\\{$PRINTER}";
     return $batCommand;
 }
-function createZip($files,$PO)
+function sendFiles($files,$PO)
 {
-    $zip = new ZipArchive;
-    $zipName = tempnam("tmp", "zip"); 
-    $zip->open($zipName, ZipArchive::CREATE);
-
-    //Getting First File and Removing It From Main Array
-    $contentPO = $files[0];
-    //Removing First Element
-    unset($files[0]);
-
-    //Creating File For First Element
-    $filename = '0.prn';
-    $zip->addFromString($filename, $contentPO);
-
-    //Creating File For Last Element
-    $filename = 'zzzzzzz.prn';
-    $zip->addFromString($filename, $contentPO);
-
-    foreach($files as $file)
-    {
-        $filename = substr(md5(microtime()),rand(0,26),7);
-        $filename .= '.prn';
-        $zip->addFromString($filename, $file);
-    }
-
-    //Adding Bat File
-    $file = createBat();
-    $zip->addFromString('print.bat', $file);
-
-    $zip->close();
-
-    header("Content-Type: application/zip");
-    header("Content-Length: " . filesize($zipName));
-    header("Content-Disposition: attachment; filename=\"{$PO}.zip\""); 	
-    readfile($zipName);
-    unlink($zipName); 
+	$filename = "{$PO}.prn";
+	
+    header('Content-Type: text/plain');  
+    header('Content-Description: File Transfer');
+    header("Content-Disposition: attachment; filename=\"{$PO}.prn\""); 	
+    
+    ob_clean();
+	flush(); 
+    
+    echo $files;
+    exit();
 }
 ?>
